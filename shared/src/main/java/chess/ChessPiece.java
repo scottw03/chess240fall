1556bbc64e7464038fdf7090c190ee032570c33f
Scott Wilson <scottw03@byu.edu>
2026-09-19 00:37:52 -0600

    Commit 8 of the project, a helper function has been created to identify viable move for the pieces

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -76,6 +76,7 @@ public class ChessPiece {
 
 
 
+
     /**
      * Calculates all the positions a chess piece can move to
      * Does not take into account moves that are illegal due to leaving the king in
